a620d172585a546369a8c7033946f94c1c2183dc
KonstantinKirillOff <53314883+KonstantinKirillOff@users.noreply.github.com>
2022-01-06 13:21:25 +0300

    Initial Commit

---
+++ MyCosts_app/MyCosts_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCosts_appApp.swift
+//  MyCosts_app
+//
+//  Created by Константин Кириллов on 06.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct MyCosts_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
